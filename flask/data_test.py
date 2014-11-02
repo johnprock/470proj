1f7519d189d2ab71429dd4e1defde45a65d177f8
Ben Sitz <bensitz@dhcp-10-202-134-75.tamulink.tamu.edu>
2014-11-02 17:23:08 -0600

    edited compute sim

--- flask/data_test.py
+++ flask/data_test.py
@@ -4,6 +4,28 @@ from flask import request
 app = Flask(__name__)
 
 def compute_sim(t1, t2):
+    t1_page = wikipedia.page(t1)
+    t2_page = wikipedia.page(t2)
+
+    t1_content = []
+    t2_content = []
+
+    t1_content = t1_page.content().split()
+    t2_content = t2_page.content().split()
+    t1_final = []
+    t2_final = []
+    x = 0
+    while text_box1[x] != '==':
+        t1_final.append(text_box1[x]) 
+        x+=1
+
+    x = 0
+    while text_box2[x] != '==':
+        t2_final.append(text_box2[x]) 
+        x+=1
+
+    t1_final = str(t1_final)
+    t2_final = str(t2_final)
     return 0
 
 @app.route("/")
