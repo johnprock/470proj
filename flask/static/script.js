7902d13dae232da1bdba3d0d22c0616123078abd
Patrick Rock <prock2011@gmail.com>
2014-11-12 15:06:54 -0600

    templated all html files for GUI overhaul

--- flask/static/script.js
+++ flask/static/script.js
@@ -1,5 +1,5 @@
 function similarity() {
-    var t1 = $('topic1').value;
-    var t2 = $('topic2').value;
-    console.log('working');
+    var t1 = $("#topic1").val();    
+    var t2 = $("#topic2").val();    
+    window.location.href = "/sim_score.html?t1=" + t1 + "&t2=" + t2 
 }
