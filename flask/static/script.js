705113bf824a5bea1a39059f66bfadea15fdf9c6
Patrick Rock <prock2011@gmail.com>
2014-11-13 11:30:52 -0600

    get topic button is working

--- flask/static/script.js
+++ flask/static/script.js
@@ -1,6 +1,10 @@
 function similarity() {
     var t1 = $("#topic1").val();    
     var t2 = $("#topic2").val();    
-    window.location.href = "/sim_score.html?t1=" + t1 + "&t2=" + t2 
+    window.location.href = "/sim_score.html?t1=" + t1 + "&t2=" + t2; 
 }
 
+function fetch() {
+    var topic = $("#topic").val();
+    window.location.href = "/?topic="+topic;
+}
